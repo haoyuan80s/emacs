a8ee3198cbe4651a3ae805f914819ca019cfd2d6
Hao Yuan <yuanha@amazon.com>
2020-07-12 10:52:37 -0700

    init

---
+++ init.el
@@ -0,0 +1,49 @@
+(require 'package)
+
+(setq gc-cons-threshold 100000000)
+
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+;; (setq package-archives
+;;       '(("melpa-stable" . "https://stable.melpa.org/packages/")
+;; 	("melpa"        . "https://melpa.org/packages/")
+;; 	;; ("GNU ELPA"     . "https://elpa.gnu.org/packages/")
+;; 	)
+;;       package-archive-priorities
+;;       '(("melpa"        . 10)
+;; 	("melpa-stable" . 5)
+;; 	;; ("GNU ELPA"     . 0)
+;; 	))
+
+;; (add-to-list 'package-pinned-packages '(cider . "MELPA Stable") t)
+
+;; Initialise the packages, avoiding a re-initialisation.
+(unless (bound-and-true-p package--initialized)
+  (setq package-enable-at-startup nil)
+  (package-initialize))
+
+;; Make sure `use-package' is available.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; Configure `use-package' prior to loading it.
+
+
+
+(eval-and-compile
+  (setq use-package-always-ensure t)
+  (setq use-package-always-defer t)
+  (setq use-package-always-demand nil)
+  (setq use-package-expand-minimally nil)
+  (setq use-package-enable-imenu-support t))
+
+(eval-when-compile
+  (require 'use-package))
+
+(require 'org)
+;; (setq vc-follow-symlinks t)
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+(add-to-list 'load-path "~/.emacs.d/lisp/")
