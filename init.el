6c5146b06236fb6bf30e4733f0b4983dbcadac53
Hao Yuan <yuanha@amazon.com>
2020-09-11 16:01:06 -0700

    asdf

--- init.el
+++ init.el
@@ -3,17 +3,17 @@
 (setq gc-cons-threshold 100000000)
 
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
-;; (add-to-list 'package-archives
-;;              '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
 
-(setq package-archives
-      '(("melpa"        . "https://melpa.org/packages/")
-	("melpa-stable" . "https://stable.melpa.org/packages/"))
-      package-archive-priorities
-      '(("melpa"        . 10)
-	("melpa-stable" . 5)))
+;; (setq package-archives
+;;       '(("melpa"        . "https://melpa.org/packages/")
+;; 	("melpa-stable" . "https://stable.melpa.org/packages/"))
+;;       package-archive-priorities
+;;       '(("melpa"        . 10)
+;; 	("melpa-stable" . 5)))
 
-(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+;; (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 
 ;; Initialise the packages, avoiding a re-initialisation.
 (unless (bound-and-true-p package--initialized)
@@ -26,9 +26,6 @@
   (package-install 'use-package))
 
 ;; Configure `use-package' prior to loading it.
-
-
-
 (eval-and-compile
   (setq use-package-always-ensure t)
   (setq use-package-always-defer t)
@@ -43,3 +40,17 @@
 ;; (setq vc-follow-symlinks t)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+;;(load-file "~/.emacs.d/_config.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(youdao-dictionary yasnippet-snippets yaml-mode whole-line-or-region which-key web-mode vterm visual-regexp-steroids use-package try treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired toml-mode tide smartparens slime-company skewer-mode scratch rustic rg rainbow-mode rainbow-delimiters python-black ox-twbs outorg org-tree-slide org-bullets olivetti nlinum multiple-cursors modus-vivendi-theme modus-operandi-theme lua-mode lsp-ui lsp-ivy ivy-hydra impatient-mode ibuffer-vc go-dlv gnu-elpa-keyring-update fullframe format-all flycheck-joker expand-region exec-path-from-shell evil eglot doom-themes doom-modeline dmenu dashboard dap-mode csv-mode crux counsel-projectile company-web clojure-snippets cider ccls beacon auto-package-update amx all-the-icons-ivy-rich)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
