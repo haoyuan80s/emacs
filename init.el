5c1c509e148605a5715523e9ec216c335fd14453
Hao Yuan <yuanha@amazon.com>
2020-09-14 21:04:59 -0700

    after debugging flycheck gopls

--- init.el
+++ init.el
@@ -40,17 +40,4 @@
 ;; (setq vc-follow-symlinks t)
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-;;(load-file "~/.emacs.d/_config.el")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(flycheck lsp-mode youdao-dictionary yasnippet-snippets yaml-mode whole-line-or-region which-key web-mode vterm visual-regexp-steroids use-package try treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired toml-mode tide smartparens slime-company skewer-mode scratch rustic rg rainbow-mode rainbow-delimiters python-black ox-twbs outorg org-tree-slide org-bullets olivetti nlinum multiple-cursors modus-vivendi-theme modus-operandi-theme lua-mode lsp-ui lsp-ivy ivy-hydra impatient-mode ibuffer-vc go-dlv gnu-elpa-keyring-update fullframe format-all flycheck-joker expand-region exec-path-from-shell evil eglot doom-themes doom-modeline dmenu dashboard dap-mode csv-mode crux counsel-projectile company-web clojure-snippets cider ccls beacon auto-package-update amx all-the-icons-ivy-rich)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; (load-file "~/.emacs.d/_config.el") 
