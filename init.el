6cc8812ca3ee8d3cafea94c78cc505a599a8bc69
Hao Yuan <yuanhao@fb.com>
2020-10-12 22:06:21 -0700

    save before trying vim

--- init.el
+++ init.el
@@ -9,10 +9,10 @@
 
 ;; (setq package-archives
 ;;       '(("melpa"        . "https://melpa.org/packages/")
-;; 	("melpa-stable" . "https://stable.melpa.org/packages/"))
+;;      ("melpa-stable" . "https://stable.melpa.org/packages/"))
 ;;       package-archive-priorities
 ;;       '(("melpa"        . 10)
-;; 	("melpa-stable" . 5)))
+;;      ("melpa-stable" . 5)))
 
 ;; (add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
 
@@ -41,16 +41,3 @@
 ;; (setq vc-follow-symlinks t)
 ;; (add-to-list 'load-path "~/.emacs.d/lisp/")
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(nlinum youdao-dictionary yasnippet-snippets yaml-mode whole-line-or-region which-key web-mode vterm visual-regexp-steroids use-package undo-tree try treemacs-projectile treemacs-magit treemacs-icons-dired toml-mode sql-indent smartparens slime-company scratch rustic rg rainbow-mode rainbow-delimiters python-black ox-twbs org-tree-slide org-bullets modus-vivendi-theme modus-operandi-theme lua-mode lsp-ui lsp-pyright lsp-ivy lsp-haskell live-py-mode key-chord js2-mode ivy-hydra ibuffer-vc haskell-snippets go-dlv format-all flycheck-pos-tip flycheck-joker expand-region exec-path-from-shell ejc-sql doom-themes doom-modeline dmenu dashboard dap-mode csv-mode crux counsel-projectile company-box clojure-snippets clojure-mode-extra-font-locking clj-refactor auto-package-update amx aggressive-indent)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
